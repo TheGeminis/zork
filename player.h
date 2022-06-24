ddf7abe7951145d3597b5a2200232974e25a8043
TheGeminis <janmarc.costa01@estudiant.upf.edu>
2022-06-24 22:15:58 +0200

    creature and player classes added

--- player.h
+++ player.h
@@ -2,6 +2,7 @@
 #define __Player__
 
 #include "creature.h"
+#include <vector>
 
 using namespace std;
 
@@ -9,6 +10,19 @@ class Player : public Creature {
 public:
 	Player(const char* name, const char* description, Room* room);
 	~Player();
+
+	bool Go(const vector<string>& args);
+	void Look(const vector<string>& args) const;
+	bool Take(const vector<string>& args);
+	bool Drop(const vector<string>& args);
+	void Inventory() const;
+	bool Equip(const vector<string>& args);
+	bool UnEquip(const vector<string>& args);
+	bool Examine(const vector<string>& args) const;
+	bool Attack(const vector<string>& args);
+	bool Loot(const vector<string>& args);
+	bool Lock(const vector<string>& args);
+	bool UnLock(const vector<string>& args);
 };
 
 #endif //__Player__
